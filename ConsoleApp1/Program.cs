78163167edbfdac7a6ee51ebe00f547df1b55b09
leehod <leehod@gmail.com>
2018-03-02 22:10:01 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("1: Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Before wait");
+            Task t = FooAsync();
+            Console.WriteLine("4: Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("After FooAsync");
+            t.Wait();
+            Console.WriteLine("5: Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("After wait");
+        }
+
+        private static async Task FooAsync()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("2: Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Done with first delay");
+            await Task.Delay(100);
+            Console.WriteLine("3: Thread: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Done with second delay");
+        }
+    }
+}
